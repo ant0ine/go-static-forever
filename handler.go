1c8710e2db0d3d5dcff79c482ff05e6a19de8b03
antoine <antoine.imbert@gmail.com>
2013-04-25 04:37:05 +0000

    Initial version

---
+++ handler.go
@@ -0,0 +1,142 @@
+// Serve files that never change
+//
+// If you can put a version string (commitish for instance) in the path of your
+// static files, then the content served by the corresponding URLs is guaranteed
+// to never change. A whole set of optimisations become possible.
+//
+// * Set the Expires and Last-Modified headers to <forever>
+// * Set the Cache-Control header to "public; max-age=<forever>; s-maxage=<forever>"
+// * Set the Etag header to the full file path ? TODO
+// * If the request contains If-Modified-Since, return 304 without checking anything
+//
+// This handler is implemented as a wrapper around http.FileServer, and when the
+// isDevelopment flag is set, http.FileServer is used directly.
+//
+// Example:
+//
+//        package main
+//
+//        import(
+//                "github.com/ant0ine/go-static-forever"
+//                "net/http"
+//        )
+//
+//        handler := forever.NewStaticHandler(
+//                http.Dir("/static/"),
+//                "1234567" // find the commitish here
+//                nil,
+//                false,
+//        )
+//
+//        http.ListenAndServe(":8080", &handler)
+//
+package forever
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type staticHandler struct {
+	fileHandler     http.Handler
+	versionPrefix   string
+	foreverHttpDate string
+	deltaSeconds    int
+	isDevelopment   bool
+}
+
+// borrowed from net/http/server.go
+const TimeFormat = "Mon, 02 Jan 2006 15:04:05 GMT"
+
+// foreverDuration default to 100 years.
+func NewStaticHandler(
+	root http.FileSystem,
+	version string,
+	foreverDuration *time.Duration,
+	isDevelopment bool) http.Handler {
+
+	// set the default
+	if foreverDuration == nil {
+		dur := time.Duration(100 * 365 * 86400 * time.Second)
+		foreverDuration = &dur
+	}
+
+	deltaSeconds := int(foreverDuration.Seconds())
+	forever := time.Now().Add(*foreverDuration)
+	foreverHttpDate := forever.Format(TimeFormat)
+
+	prefix := "/" + version
+
+	return &staticHandler{
+		fileHandler:     http.FileServer(root),
+		versionPrefix:   prefix,
+		foreverHttpDate: foreverHttpDate,
+		deltaSeconds:    deltaSeconds,
+		isDevelopment:   isDevelopment,
+	}
+}
+
+func (self *staticHandler) ServeHTTP(origWriter http.ResponseWriter, origRequest *http.Request) {
+
+	if !strings.HasPrefix(origRequest.URL.Path, self.versionPrefix) {
+		http.NotFound(origWriter, origRequest)
+		return
+	}
+
+	origRequest.URL.Path = origRequest.URL.Path[len(self.versionPrefix):]
+
+	if self.isDevelopment {
+		self.fileHandler.ServeHTTP(origWriter, origRequest)
+		return
+	}
+
+	// If the request contains If-Modified-Since, return 304 without checking anything
+	if origRequest.Header.Get("If-Modified-Since") != "" {
+		http.Error(origWriter, "Not Modified", http.StatusNotModified)
+		return
+	}
+
+        // Provide writer wrapper to write the custom headers only when the response code is 200
+        writer := &responseWriter{
+                origWriter,
+                self,
+                false,
+        }
+
+	self.fileHandler.ServeHTTP(writer, origRequest)
+}
+
+
+// Inherit from an object implementing the http.ResponseWriter interface
+type responseWriter struct {
+	http.ResponseWriter
+        handler *staticHandler
+	wroteHeader bool
+}
+
+// Overloading of the http.ResponseWriter method.
+func (self *responseWriter) WriteHeader(code int) {
+        if code == 200 {
+                // Cache forever
+                self.Header().Set("Expires", self.handler.foreverHttpDate)
+                self.Header().Set("Last-Modified", self.handler.foreverHttpDate)
+                self.Header().Set("Cache-Control", fmt.Sprintf(
+                        "public; max-age=%d; s-maxage=%d",
+                        self.handler.deltaSeconds,
+                ))
+                // TODO Set the Etag header to the full file path ?
+        }
+	self.ResponseWriter.WriteHeader(code)
+	self.wroteHeader = true
+}
+
+// Overloading of the http.ResponseWriter method.
+func (self *responseWriter) Write(b []byte) (int, error) {
+	if !self.wroteHeader {
+		self.WriteHeader(http.StatusOK)
+	}
+	return self.ResponseWriter.Write(b)
+}
+
---
+++ handler_test.go
@@ -0,0 +1,132 @@
+package forever
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func codeIs(t *testing.T, r *httptest.ResponseRecorder, expectedCode int) {
+	if r.Code != expectedCode {
+		t.Errorf("Code %d expected, got: %d", expectedCode, r.Code)
+	}
+}
+
+func TestWrongVersion(t *testing.T) {
+        // wring version string, expect a 404
+
+	handler := NewStaticHandler(
+		http.Dir("."),
+		"1234567",
+		nil,
+		true,
+	)
+
+	urlObj, err := url.Parse("http://1.2.3.4/wrong_version/handler.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := http.Request{
+		Method: "GET",
+		URL:    urlObj,
+	}
+	r.Header = http.Header{}
+
+	recorder := httptest.NewRecorder()
+
+	handler.ServeHTTP(recorder, &r)
+
+	codeIs(t, recorder, 404)
+}
+
+func TestDevelopement(t *testing.T) {
+        // right version string, expect a 200
+
+	handler := NewStaticHandler(
+		http.Dir("."),
+		"1234567",
+		nil,
+		true,
+	)
+
+	urlObj, err := url.Parse("http://1.2.3.4/1234567/handler.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := http.Request{
+		Method: "GET",
+		URL:    urlObj,
+	}
+	r.Header = http.Header{}
+
+	recorder := httptest.NewRecorder()
+
+	handler.ServeHTTP(recorder, &r)
+
+	codeIs(t, recorder, 200)
+}
+
+func TestFirstRequest(t *testing.T) {
+        // expect a 200 with all the custom headers set
+
+	handler := NewStaticHandler(
+		http.Dir("."),
+		"1234567",
+		nil,
+		false,
+	)
+
+	handler = handler.(*staticHandler)
+
+	urlObj, err := url.Parse("http://1.2.3.4/1234567/handler.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := http.Request{
+		Method: "GET",
+		URL:    urlObj,
+	}
+	r.Header = http.Header{}
+
+	recorder := httptest.NewRecorder()
+
+	handler.ServeHTTP(recorder, &r)
+
+	codeIs(t, recorder, 200)
+
+	expires := recorder.HeaderMap.Get("Expires")
+	if expires == "" {
+		t.Errorf("Expires expected, got: %s", expires)
+	}
+}
+
+func TestSecondRequest(t *testing.T) {
+        // sent with If-Modified-Since, expect a 304
+
+	handler := NewStaticHandler(
+		http.Dir("."),
+		"1234567",
+		nil,
+		false,
+	)
+
+	handler = handler.(*staticHandler)
+
+	urlObj, err := url.Parse("http://1.2.3.4/1234567/handler.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := http.Request{
+		Method: "GET",
+		URL:    urlObj,
+	}
+	r.Header = http.Header{}
+        r.Header.Set("If-Modified-Since", "Sat, 01 Apr 2113 04:15:01 GMT")
+
+	recorder := httptest.NewRecorder()
+
+	handler.ServeHTTP(recorder, &r)
+
+	codeIs(t, recorder, 304)
+}
